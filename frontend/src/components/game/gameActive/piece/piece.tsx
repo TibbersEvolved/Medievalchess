d31eeda18f946fd171fe967d1d2cabd476c4879b
TibbersEvolved <erik.olin.spanga@gmail.com>
2024-11-20 20:15:47 +0100

    Added display indicators for active turn

--- frontend/src/components/game/gameActive/piece/piece.tsx
+++ frontend/src/components/game/gameActive/piece/piece.tsx
@@ -8,14 +8,26 @@ export default function GamePiece(props: pieceType) {
   return (
     <>
       <img
-        className="absolute piece z-40 mt-3 pointer-events-none"
+        className="absolute piece z-40 mt-2 pointer-events-none"
         src={getPieceImage(props.type, props.owner)}
       ></img>
+      {props.active == 2 && (
+        <img
+          className="absolute size-3 mt-2 z-30 pointer-events-none"
+          src="src/assets/tiles/activeMovement.png"
+        ></img>
+      )}
+      {props.active == 1 && (
+        <img
+          className="absolute size-3 mt-2 z-30 pointer-events-none"
+          src="src/assets/tiles/activeAttack.png"
+        ></img>
+      )}
       <div className="flex flex-row mt-2 gap-1 pointer-events-none justify-center">
         {temp.map((t) => (
           <img
             key={t}
-            className="size-2  pointer-events-none z-40 "
+            className="size-2 pointer-events-none z-40 "
             src="src/assets/resources/hearth.png"
           ></img>
         ))}
